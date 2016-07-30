776dfb3cc92147a520314f13a8bc26a05587fde4
Lindsay Evans <lindsay.evans@boomworks.com.au>
2016-07-30 10:06:01 +1000

    Getting basic A-Frame scene happening

---
+++ beard-science/public/config.js
@@ -0,0 +1,29 @@
+System.config({
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: "typescript",
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  },
+
+  packages: {
+    "js": {
+      "defaultExtension": "ts"
+    }
+  },
+
+  map: {
+    "aframevr/aframe": "github:aframevr/aframe@0.2.0",
+    "typescript": "npm:typescript@1.8.10",
+    "github:jspm/nodelibs-os@0.1.0": {
+      "os-browserify": "npm:os-browserify@0.1.2"
+    },
+    "npm:os-browserify@0.1.2": {
+      "os": "github:jspm/nodelibs-os@0.1.0"
+    },
+    "npm:typescript@1.8.10": {
+      "os": "github:jspm/nodelibs-os@0.1.0"
+    }
+  }
+});
